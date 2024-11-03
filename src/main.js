eb74330b9fe9e971f739d72aff72a444b5c9c74f
vitayusko <vitayusko@users.noreply.github.com>
2024-11-03 22:41:24 +0100

    Burger menu btn add js

--- src/main.js
+++ src/main.js
@@ -9,3 +9,23 @@ var swiper = new Swiper('.mySwiper', {
     clickable: true,
   },
 });
+
+// Mobile menu work
+
+const mobileMenuButton = document.querySelector('.mobile-menu-btn');
+const closeMenuButton = document.querySelector('.close-menu-btn');
+const mobileMenu = document.querySelector('.mobile-menu-bg');
+
+if (mobileMenuButton && closeMenuButton && mobileMenu) {
+  const openMenu = () => {
+    mobileMenu.classList.add('is-open');
+  };
+
+  const closeMenu = () => {
+    mobileMenu.classList.remove('is-open');
+  };
+
+  mobileMenuButton.addEventListener('click', openMenu);
+
+  closeMenuButton.addEventListener('click', closeMenu);
+}
